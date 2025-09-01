3a0188c7d0d242c62288d87942f3b44999afbbb0
kaayush <aayushrocks@789@gmail.com>
2025-09-01 20:48:21 +0530

    brand creation,brand list

--- ECOMMERCE/MainProject/inventory/views.py
+++ ECOMMERCE/MainProject/inventory/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from .models import Product
-from .forms import ProductCreateForm
+from .models import Product,Brand,Category
+from .forms import ProductCreateForm,BrandCreateForm,CategoryCreateForm
 # Create your views here.
 def product_list(request):
     products = Product.objects.all()
@@ -8,7 +8,25 @@ def product_list(request):
         'products':products
     }
     return render(request,'home.html',context)
- 
+# def brand_list(request):
+#     brand = Brand.objects.all()
+#     context = {
+#         'brand':brand
+#     }
+#     return render(request,'brandfor.html',context)
+# def category_list(request):
+#     category = Category.objects.all()
+#     context = {
+#         'category':category
+#     }
+#     return render(request,'brandfor.html',context)
+def brand_list_view(request):
+    brands = Brand.objects.all()
+    return render(request, 'inventory/brandfor.html', {'brands': brands})
+def brand_details_view(request, id):
+    brand = get_object_or_404(Brand, id=id)
+    return render(request, 'inventory/brands_detail.html', {'brand': brand})
+
 def create_product(request):
     form = ProductCreateForm()
     if request.method == "GET":
@@ -47,3 +65,27 @@ def delete_product(request,id):
     elif request.method == "POST":
         product.delete()
     return redirect('product_list')
+
+def create_brand(request):
+    if request.method == "POST":
+        form = BrandCreateForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = BrandCreateForm()
+    return render(request, 'inventory/brandcreate.html', {'form': form})
+
+def create_category(request):
+    form = CategoryCreateForm()
+    if request.method == "GET":
+        return render(request,'inventory/category_c.html',{'form':form})
+    elif request.method == "POST":
+        form = CategoryCreateForm(request.POST,request.FILES)
+        if form.is_valid(): 
+            form.save()
+            return redirect('product_list')
+        else:
+            return render(request,'inventory/category_c.html',{'form':form})
+    else:
+        return render(request,'inventory/category_c.html',{'form':form})
